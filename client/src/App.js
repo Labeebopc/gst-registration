e5dcfe022fd727930bbf61217856c7528557fe2d
Labeebopc <Labeebopc313@gmail.com>
2023-03-11 07:21:00 +0530

    alert box component design completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 import './App.css';
+//import CustomAlert from './components/customAlert/customAlert';
 import Registration from './components/registration/register';
 
 function App() {
   return (
     <>
-    <Registration/>
+
+      {/* <CustomAlert /> */}
+      <Registration />
+
     </>
   );
 }
